328bfa21a079924ca0340348018e15146ebe1408
edward <edukmusyoka@gmail.com>
2026-02-28 16:10:26 +0300

    initialcommit

---
+++ utilities/decorators.py
@@ -0,0 +1,23 @@
+
+from colorama import Fore, Style
+
+def login_required(func):
+    """Checks if a user is logged in."""
+    def wrapper(current_user, *args, **kwargs):
+        if current_user is None:
+            print(Fore.RED + "Error: You must login first!" + Style.RESET_ALL)
+            return # Stop the function
+        return func(current_user, *args, **kwargs)
+    return wrapper
+
+def admin_required(func):
+    """Checks if the user is an Admin."""
+    def wrapper(current_user, *args, **kwargs):
+        if current_user is None:
+            print(Fore.RED + "Error: Login required." + Style.RESET_ALL)
+            return
+        if current_user.role != 'admin':
+            print(Fore.RED + "Error: Admins only." + Style.RESET_ALL)
+            return
+        return func(current_user, *args, **kwargs)
+    return wrapper
