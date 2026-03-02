41ded9963c101b89544d8eee66be74649b57c60c
Halima <h4limaabdi@gmail.com>
2026-03-02 16:55:07 +0300

    Implemented one-to-many relationship between users and tasks

--- services/task_service.py
+++ services/task_service.py
@@ -1,4 +1,4 @@
-from models import Task
+from models import Task,User
 from utilities.helpers import load_data, save_data
 
 def create_task(title, description, owner_email):
@@ -15,6 +15,18 @@ def create_task(title, description, owner_email):
 
     data["tasks"].append(task_dict)
     save_data(data)
+
+    #attach task to corresponding user
+    for user_dict in data['users']:
+        if user_dict['email'] ==owner_email:
+            user = User(
+                username=user_dict['username'],
+                email=user_dict['email'],
+                password=user_dict['password'],
+                role=user_dict['role']
+            )
+            new_task.assign_to(user)
+            break
 
     return "Task created successfully"
 
@@ -35,14 +47,44 @@ def delete_task(title, owner_email):
     data["tasks"] = updated_tasks
     save_data(data)
 
+    #remove task from user's _task list
+    for user_dict in data["users"]: 
+        if user_dict["email"] == owner_email: 
+            user = User( 
+                username=user_dict["username"], 
+                email=user_dict["email"], 
+                password=user_dict["password"], 
+                role=user_dict["role"] 
+                ) 
+            # Find the task object to remove 
+            for t in user.tasks: 
+                if t.title == title and t._assigned_to == owner_email: 
+                    user.remove_task(t) 
+                    break 
+                break
+
+
     return "Task deleted"
 
 
-def get_task_by_user(owner_email):
-    data = load_data()
-
-    return [
-        Task.from_dict(task)
-        for task in data["tasks"]
-        if task["assigned_to"] == owner_email
-    ]
+def get_task_by_user(owner_email): 
+        data = load_data() 
+        
+        # Find the user 
+        for user_dict in data["users"]: 
+            if user_dict["email"] == owner_email: 
+                user = User( 
+                    username=user_dict["username"], 
+                    email=user_dict["email"], 
+                    password=user_dict["password"], 
+                    role=user_dict["role"] 
+                ) 
+                    
+                # Populate user's _tasks from persisted data 
+                user._tasks = [ 
+                    Task.from_dict(task) 
+                    for task in data["tasks"] 
+                    if task["assigned_to"] == owner_email 
+                ] 
+                return user.tasks 
+            return [] 
